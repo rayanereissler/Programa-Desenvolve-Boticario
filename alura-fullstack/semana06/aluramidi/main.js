dfe24ba678a728dd2bde62ad320693f7ad9adc0d
perkles <reisslertec@gmail.com>
2022-03-03 23:00:24 -0300

    inicio semana 05

---
+++ alura-fullstack/semana06/aluramidi/main.js
@@ -0,0 +1,12 @@
+function tocasompom () {
+    document.querySelector('#som_tecla_pom').play();
+}
+
+document.querySelector('.tecla_pom').onclick = tocasompom;
+
+function tocasompom () {
+    document.querySelector('#som_tecla_pom').play();
+}
+
+document.querySelector('.tecla_pom').onclick = tocasompom;
+
